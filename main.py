ec9c43746ce4b2ba4ee87d441c81a2586e95008a
Rami Zoey <ramizoey@ramis-air.ssw.umb.edu>
2025-12-04 16:15:40 -0500

    learn oop

---
+++ main.py
@@ -0,0 +1,25 @@
+class Item:
+    def __init__(self, name, price, quantity=0):
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+
+    def caculate_total_price(self):
+        return self.price * self.quantity
+
+item1 = Item("Phone", "100", 1)
+
+item2 = Item("Laptop", "1000", 3)
+
+print(item1.caculate_total_price())
+print(item2.caculate_total_price())
+
+#continue at 25 seconds 
+
+"""
+print(type(item1))
+print(type(item1.name))
+print(type(item1.price))
+print(type(item1.quantity))
+"""
+
