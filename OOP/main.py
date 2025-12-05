f8ea12979320c61b720b5840127498321fca3e31
Rami  Huu Nguyen <ramihuunguyen@Rami.local>
2025-12-05 11:29:38 -0500

    yes

--- OOP/main.py
+++ OOP/main.py
@@ -1,22 +1,40 @@
 """ Source: https://github.com/jimdevops19/PythonOOP/blob/main/01%20-%20Introduction%20to%20OOP/main.py"""
 
 class Item:
-    def __init__(self, name, price, quantity=0):
+    pay_rate = 0.8 # The pay rate after 20% discount
+    def __init__(self, name: str, price: float, quantity=0):
+
+        # Assign to self object
         self.name = name
         self.price = price
         self.quantity = quantity
 
     def caculate_total_price(self):
         return self.price * self.quantity
+    
+    def apply_discount(self):
+        self.price = self.price * self.pay_rate
 
-item1 = Item("Phone", "100", 1)
+item1 = Item("Phone", 100, 1)
 
 item2 = Item("Laptop", "1000", 3)
 
-print(item1.caculate_total_price())
-print(item2.caculate_total_price())
+item1.apply_discount()
 
-#continue at 25 seconds 
+#print(item1.price)
+
+#print(item1.caculate_total_price())
+#print(item2.caculate_total_price())
+
+#print(Item.__dict__) # All the attributes of the class
+#print(item1.pay_rate)
+#print(item2.pay_rate)
+
+item1 = Item("Phone", 100, 1)
+item2 = Item("Laptop", 1000, 3)
+item3 = Item("Cable", 10, 5)
+item4 = Item("Mouse", 50, 5)
+item5 = Item("Keyboard", 75, 5)
 
 """
 print(type(item1))
